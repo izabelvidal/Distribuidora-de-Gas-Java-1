e72efc4112afeb3117d46a68199c33935159981f
LeticiaAraujo26 <leticiaaraujo6392@gmail.com>
2020-10-18 20:34:00 -0300

    iniciando gui

--- src/gui/controllers/MenuInicialController.java
+++ src/gui/controllers/MenuInicialController.java
@@ -4,23 +4,81 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+import javax.swing.*;
+
 
 public class MenuInicialController implements Initializable {
-
     @FXML
-    public Pane painel;
+    private Pane painel;
     /**
-     * Initializes the controller class.
+     * Initializes the controller class
      */
     public MenuInicialController(){
 
     }
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle rb){
         //TODO
     }
 
+    @FXML
+    private void vendaBtnHandler(ActionEvent event){
+        Pane telaVenda;
+        try{
+            telaVenda = FXMLLoader.load(getClass().getResource("../views/venda/TelaVenda.fxml"));
+            painel.getChildren().setAll(telaVenda);
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 
+    @FXML
+    private void cadastroBtnHandler(ActionEvent event){
+        Pane telaCadastro;
+        try{
+            telaCadastro = FXMLLoader.load(getClass().getResource("../views/cadastro/TelaCadastro.fxml"));
+            painel.getChildren().setAll(telaCadastro);
+        }catch (IOException ex){
+            Logger.getLogger(MenuInicialController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+
+    @FXML
+    private void alterarBtnHandler(ActionEvent event){
+        Pane telaAlterar;
+        try{
+            telaAlterar = FXMLLoader.load(getClass().getResource("../views/alterar/TelaAlterar.fxml"));
+            painel.getChildren().setAll(telaAlterar);
+        }catch (IOException ex){
+            Logger.getLogger(MenuInicialController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+
+    @FXML
+    private void listarBtnHandler(ActionEvent event){
+        Pane telaListar;
+        try{
+            telaListar = FXMLLoader.load(getClass().getResource("../views/consultas/TelaListar.fxml"));
+            painel.getChildren().setAll(telaListar);
+        }catch (IOException ex){
+            Logger.getLogger(MenuInicialController.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+
+    @FXML
+    private void sairBtnHandler(ActionEvent event){
+        System.exit(0);
+    }
 }
--- src/gui/controllers/cadastro/TelaCadastroClienteController.java
+++ src/gui/controllers/cadastro/TelaCadastroClienteController.java
@@ -1,4 +1,90 @@
 package gui.controllers.cadastro;
 
-public class TelaCadastroClienteController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+public class TelaCadastroClienteController implements Initializable {
+    private Alert spam;
+    private ObservableList<String> choicesList;
+
+    @FXML
+    private Pane painelCadastroCliente;
+    @FXML
+    private TextField inputNome;
+    @FXML
+    private TextField inputTelefone;
+    @FXML
+    private Button btnConfirmar;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private DatePicker dataNascimento;
+    @FXML
+    private TextField inputCpf;
+    @FXML
+    private ChoiceBox choiceTipo;
+    @FXML
+    private TextField inputRua;
+    @FXML
+    private TextField inputBairro;
+    @FXML
+    private TextField inputCidade;
+    @FXML
+    private TextField inputEstado;
+    @FXML
+    private TextField inputNumeroDaCasa;
+
+    public TelaCadastroClienteController(){
+        spam = new Alert(Alert.AlertType.NONE);
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> choicesList = FXCollections.observableArrayList("Comum", "Revendedor");
+        choiceTipo.setItems(choicesList);
+        LocalDate maxDate = LocalDate.now();
+        dataNascimento.setDayCellFactory(d ->
+                new DateCell(){
+                @Override public void updateItem(LocalDate item, boolean empty){
+                    super.updateItem(item,empty);
+                    setDisable(item.isAfter(maxDate));
+                }
+                });
+        //TODO
+    }
+
+    @FXML
+    private void confimarBtnHandler(ActionEvent event){
+        String nome = inputNome.getText();
+        String cpf = inputCpf.getText();
+        String tel = inputTelefone.getText();
+        String tipo = (String) choiceTipo.getValue();
+        LocalDate data = dataNascimento.getValue();
+        String rua = inputRua.getText();
+        String bairro = inputBairro.getText();
+        String cidade = inputCidade.getText();
+        String estado = inputEstado.getText();
+        String numero = inputNumeroDaCasa.getText();
+
+        if((nome.length()>0) && (cpf.length()>0) && (tel.length()>0)
+        && (choiceTipo.getValue() != null) && (rua.length()>0) && (bairro.length()>0)
+        && (cidade.length()>0) && (estado.length()>0) && (numero.length()>0)
+        && (dataNascimento.getValue() != null));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataNasc = data.format(formatter);
+
+
+
+
+
+    }
 }
