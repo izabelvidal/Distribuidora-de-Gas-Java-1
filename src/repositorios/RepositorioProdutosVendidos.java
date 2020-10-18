d7c72f359b3433916e1c30a3e95230086731e096
LeticiaAraujo26 <leticiaaraujo6392@gmail.com>
2020-10-17 21:15:56 -0300

    adicionando comentários, métodos nos repositórios

--- src/repositorios/RepositorioProdutosVendidos.java
+++ src/repositorios/RepositorioProdutosVendidos.java
@@ -1,15 +1,27 @@
 package repositorios;
 
+import entidades.Cliente;
+import entidades.Produto;
 import entidades.Venda;
+import excecoes.ProdutoInexistenteException;
+import excecoes.QuantidadeInvalidaException;
 
 import java.util.ArrayList;
+
+/**
+ * Essa classe é responsável por armazenar e gerenciar os Produtos vendidos
+ *
+ * @author Letícia Araújo
+ */
 
 public class RepositorioProdutosVendidos implements iRepositorioProdutosVendidos{
 
     private ArrayList<Venda> produtosVendidos;
+    private RepositorioProdutos repositorioProdutos;
 
-    public RepositorioProdutosVendidos(){
+    public RepositorioProdutosVendidos(RepositorioProdutos repositorioProdutos){
         this.produtosVendidos = new ArrayList<>();
+        this.repositorioProdutos = repositorioProdutos;
     }
 
     @Override
@@ -17,6 +29,22 @@ public class RepositorioProdutosVendidos implements iRepositorioProdutosVendidos
         this.produtosVendidos.add(produtoVendido);
     }
 
+    /*public void decrementarQntd(String id, int qntd, String cpf) throws QuantidadeInvalidaException, ProdutoInexistenteException{
+        Produto produto = this.consultarProduto(id);
+        Cliente cliente = this.consultarCliente(cpf);
+
+        if(produto.getQuantidade() < qntd){
+            throw new QuantidadeInvalidaException(qntd);
+        }else{
+            produto.setQuantidade(produto.getQuantidade() - qntd);
+
+            Venda venda = new Venda(qntd, new Produto(produto.getNome(),produto.getMarca(),produto.getId(),produto.getQuantidade(), produto.getPeso(), produto.getPreco() * qntd), );
+        }
+    }*/
+
+    public Produto consultarProduto(String id) throws ProdutoInexistenteException{
+        return this.repositorioProdutos.getProduto(id);
+    }
     @Override
     public ArrayList<Venda> listarProdutosVendidos() {
         return this.produtosVendidos;
