bc2bc4031c095431cb3591ffda7b8a0dd7f986a8
LeticiaAraujo26 <leticiaaraujo6392@gmail.com>
2020-10-18 16:36:06 -0300

    iniciando gui

--- src/negocio/entidades/Venda.java
+++ src/negocio/entidades/Venda.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Essa classe representa a venda dos produtos.
+ * Essa classe representa a gui.views.venda dos produtos.
  *
  * @author Letícia Araújo
  */
