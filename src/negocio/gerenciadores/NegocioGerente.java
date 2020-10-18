c0d233f16405f16a195392c35d7e660e57ca7a95
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-18 14:04:28 -0300

    cadastrar sem verificações

--- src/negocio/gerenciadores/NegocioGerente.java
+++ src/negocio/gerenciadores/NegocioGerente.java
@@ -1,9 +1,16 @@
 package negocio.gerenciadores;
 
+import dados.RepositorioGerente;
+import dados.contratos.iRepositorioPessoa;
 import negocio.entidades.Endereco;
 import negocio.entidades.Gerente;
+import negocio.entidades.Pessoa;
+import negocio.excecoes.CnpjApenasNumerosException;
+import negocio.excecoes.CnpjTamanhoException;
 import negocio.excecoes.SenhaCaracteresInvalidosException;
 import negocio.excecoes.SenhaTamanhoException;
+
+import java.util.Scanner;
 
 public class NegocioGerente extends Gerente {
     public NegocioGerente(String nome, String cpf, String dataNascimento, Endereco endereco, String email, String senha, String cnpj){
@@ -17,5 +24,56 @@ public class NegocioGerente extends Gerente {
         if(!this.getSenha().matches("[a-zA-Z_0-9]")){
             throw new SenhaCaracteresInvalidosException(this.getSenha());
         }
+    }
+
+    public void validarCnpj() throws CnpjApenasNumerosException, CnpjTamanhoException{
+        boolean contemLetra;
+        char[] cnpjArray = this.getCnpj().toCharArray();
+
+        if(this.getCnpj().length() != 14){
+            throw new CnpjTamanhoException(this.getCnpj());
+        }
+        for(int i =0; i < this.getCnpj().length(); i++){
+            contemLetra = Character.isLetter(cnpjArray[i]);
+            if(contemLetra){
+                throw new CnpjApenasNumerosException(this.getCnpj());
+            }
+        }
+    }
+
+    public void cadastrar(){
+        Scanner sc = new Scanner(System.in);
+        iRepositorioPessoa rep = new RepositorioGerente();
+        System.out.println("------------- Cadastrar Gerente ----------------\n");
+
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Data de nascimento: ");
+        String dataNascimento = sc.nextLine();
+        System.out.print("CNPJ: ");
+        String cnpj = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("Senha: ");
+        String senha = sc.nextLine();
+
+        System.out.println("------------- Cadastrar Endereço ----------------\n");
+        System.out.print("Rua: ");
+        String rua = sc.nextLine();
+        System.out.print("Número: ");
+        int numero = sc.nextInt();
+        System.out.print("Bairro: ");
+        String bairro = sc.nextLine();
+        System.out.print("Cidade: ");
+        String cidade = sc.nextLine();
+        System.out.print("Estado: ");
+        String estado = sc.nextLine();
+
+        Endereco endereco = new Endereco(rua, numero, bairro, cidade, estado);
+
+        Pessoa gerente = new Gerente(nome, cpf, dataNascimento, endereco, email, senha, cnpj);
+        rep.adicionarPessoa(gerente);
     }
 }
