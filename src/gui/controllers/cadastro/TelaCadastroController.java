be4c891e1fad1c1936cf110c200ebc1f3fdd4ecd
LeticiaAraujo26 <leticiaaraujo6392@gmail.com>
2020-10-18 17:53:38 -0300

    iniciando gui

---
+++ src/gui/controllers/cadastro/TelaCadastroController.java
@@ -0,0 +1,10 @@
+package gui.controllers.cadastro;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class TelaCadastroController {
+    @FXML
+    public Pane painelCadastro;
+
+}
---
+++ src/gui/controllers/cadastro/TelaCadastroProdutoController.java
@@ -0,0 +1,4 @@
+package gui.controllers.cadastro;
+
+public class TelaCadastroProdutoController {
+}
